88cd5c8cf9e8f5e3f82c9fa9b9f89ed1f450c088
gheofvens <gheofvens@mail.ru>
2025-08-24 11:15:24 +0300

    Очистка репозитория

--- src/components/ui/Input.jsx
+++
@@ -1,8 +0,0 @@
-export function Input({ className = "", ...props }) {
-  return (
-    <input
-      {...props}
-      className={`px-3 py-2 rounded-lg border focus:outline-none focus:ring ${className}`}
-    />
-  );
-}
